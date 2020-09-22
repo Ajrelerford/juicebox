422fd354b88db8f85eefbb7b878971dc8da2a183
Ajrelerford <ajrelerford@sbcglobal.net>
2020-09-21 20:00:18 -0700

    Module 1 of Juicebox pt. 2

---
+++ index.js
@@ -0,0 +1,27 @@
+const PORT = 8000;
+const express = require("express");
+const server = express();
+
+const bodyParser = require("body-parser");
+server.use(bodyParser.json());
+
+const { client } = require('./db');
+client.connect();
+
+const morgan = require("morgan");
+server.use(morgan("dev"));
+
+server.use((req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+
+  next();
+});
+
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
+
+server.listen(PORT, () => {
+  console.log("The server is up on port", PORT);
+});
